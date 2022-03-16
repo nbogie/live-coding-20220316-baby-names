db5aca5eb622167ae1aa1da61501b74f9a6126dd
nbogie <neillbogie@googlemail.com>
2022-03-16 13:30:09 +0000

    import from babyNames.json and show names sorted alphabetically (naive)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import allBabyNames from './data/babyNames.json';
 
 function App() {
+  const namesToShow: string[] = allBabyNames.map(oneInfo => oneInfo.name);
+  namesToShow.sort();
+
   return (
     <div className="App">
       Baby Names (live coded)
+
+      {namesToShow.map(name => <div>{name}</div>)}
     </div>
   );
 }
