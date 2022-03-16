f95fa7a026f6ce87958d155d0b1261407e398efe
nbogie <neillbogie@googlemail.com>
2022-03-16 14:09:33 +0000

    sort name objects correctly.  add type for name objects

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.css';
 import allBabyNames from './data/babyNames.json';
 
-function App() {
-  // namesToShow.sort();
+interface BabyNameInfo {
+  name: string;
+  id: number;
+  sex: string;
+}
 
+function compareTwoBabyNameInfos(infoA: BabyNameInfo, infoB: BabyNameInfo) {
+  if (infoA.name < infoB.name) {
+    return -1;
+  } else if (infoA.name > infoB.name) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function App() {
+  const sortedBabyNames: BabyNameInfo[] = [...allBabyNames];
+
+
+  sortedBabyNames.sort(compareTwoBabyNameInfos);
   return (
+
     <div className="App">
       Baby Names (live coded)
 
       <div className="babyNamesList">
-        {allBabyNames.map(nameInfo => (
+        {sortedBabyNames.map(nameInfo => (
           <div
             className={"babyName " + nameInfo.sex}
             key={nameInfo.id}
