21d081ee6b873da389083804e57574716e08dc86
nbogie <neillbogie@googlemail.com>
2022-03-16 13:39:03 +0000

    simple styling for m and f names

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import './App.css';
 import allBabyNames from './data/babyNames.json';
 
 function App() {
-  const namesToShow: string[] = allBabyNames.map(oneInfo => oneInfo.name);
-  namesToShow.sort();
+  // namesToShow.sort();
 
   return (
     <div className="App">
       Baby Names (live coded)
 
-      {namesToShow.map(name => <div>{name}</div>)}
+      {allBabyNames.map(nameInfo => (
+        <div
+          className={"babyName " + nameInfo.sex}
+          key={nameInfo.id}
+        >{nameInfo.sex} - {nameInfo.name}</div>
+      )
+      )}
     </div>
   );
 }
