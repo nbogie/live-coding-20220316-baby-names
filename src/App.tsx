1ca31b24ae011751dc0d144503d1906d25c14b57
nbogie <neillbogie@googlemail.com>
2022-03-17 14:10:01 +0000

    ex 2: add search bar (case insensitive)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BabyNameInfo } from './types';
 import { compareTwoBabyNameInfos } from './BabyNameUtils';
@@ -6,16 +6,36 @@ import allBabyNames from './data/babyNames.json';
 
 function App() {
   const sortedBabyNames: BabyNameInfo[] = [...allBabyNames];
-
-
   sortedBabyNames.sort(compareTwoBabyNameInfos);
+  const [searchTerm, setSearchTerm] = useState("");
+  const namesToShow: BabyNameInfo[] = sortedBabyNames.filter(doesSearchTermOccurInName);
+
+  function handleSearchTermChanged(event: any) {
+    setSearchTerm(event.target.value);
+  }
+
+  function doesSearchTermOccurInName(nameInfo: BabyNameInfo): boolean {
+    return nameInfo.name.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+
   return (
 
     <div className="App">
       Baby Names (live coded)
 
+      <input
+        placeholder="Type your search"
+        value={searchTerm}
+        onChange={handleSearchTermChanged}
+      />
+      You are searching for {searchTerm}
+
+      <hr />
+
+      Now showing {namesToShow.length} names out of {sortedBabyNames.length} possible names.
+
       <div className="babyNamesList">
-        {sortedBabyNames.map(nameInfo => (
+        {namesToShow.map(nameInfo => (
           <div
             className={"babyName " + nameInfo.sex}
             key={nameInfo.id}
